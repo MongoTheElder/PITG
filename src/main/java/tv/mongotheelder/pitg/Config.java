b1d18efe14df6869557812cc2f14d015655a77d0
MongoTheElder <mongotheelder@gmail.com>
2020-07-02 18:31:16 -0400

    Add new features to the Glazing Tool and allow PITG Glass Panes to be unbreakable

--- src/main/java/tv/mongotheelder/pitg/Config.java
+++ src/main/java/tv/mongotheelder/pitg/Config.java
@@ -13,6 +13,8 @@ public class Config {
 
     public static ForgeConfigSpec.BooleanValue ENABLE_GREEN_DYE;
     public static ForgeConfigSpec.BooleanValue ENABLE_GLAZING_TOOL;
+    public static ForgeConfigSpec.BooleanValue ENABLE_PANE_BREAK;
+    public static ForgeConfigSpec.BooleanValue ENABLE_UNBREAKABLE;
 
     static {
         ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
@@ -24,8 +26,10 @@ public class Config {
     private static void setupItems(ForgeConfigSpec.Builder COMMON_BUILDER) {
         COMMON_BUILDER.comment("Glazing Tool settings").push(CATEGORY_ITEMS);
 
-        ENABLE_GLAZING_TOOL = COMMON_BUILDER.comment("Enable the glazing tool").define("glazing_tool_enabled", true);
-        ENABLE_GREEN_DYE = COMMON_BUILDER.comment("Enable alternate green dye").define("green_dye_enabled", true);
+        ENABLE_GLAZING_TOOL = COMMON_BUILDER.comment(" Enable the glazing tool").define("glazing_tool_enabled", true);
+        ENABLE_PANE_BREAK = COMMON_BUILDER.comment(" Let the glazing tool drop panes (like silk touch) by sneaking while in BREAK mode").define("glazing_tool_break_pane", false);
+        ENABLE_UNBREAKABLE= COMMON_BUILDER.comment(" Allow glazing tool to mark glass panes as unbreakable while in UNBREAKING mode").define("unbreakable_pane", false);
+        ENABLE_GREEN_DYE = COMMON_BUILDER.comment(" Enable alternate green dye").define("green_dye_enabled", true);
 
         COMMON_BUILDER.pop();
     }
