420df91fb96b4521bf1e86a5d9bb06ab321236ee
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:20:16 -0400

    Custom recipe providers to support QoL blocks (Glass Kiln and Glass Pane Table)

---
+++ src/main/java/tv/mongotheelder/pitg/recipes/GlassPaneTableRecipe.java
@@ -0,0 +1,23 @@
+package tv.mongotheelder.pitg.recipes;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.SingleItemRecipe;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import tv.mongotheelder.pitg.setup.Registration;
+
+public class GlassPaneTableRecipe extends SingleItemRecipe {
+    public GlassPaneTableRecipe(ResourceLocation p_i50021_1_, String p_i50021_2_, Ingredient ingredient, ItemStack stack) {
+        super(Registration.GLASS_PANE_TABLE_TYPE, Registration.GLASS_PANE_TABLE_RECIPE_SERIALIZER.get(), p_i50021_1_, p_i50021_2_, ingredient, stack);
+    }
+
+    public boolean matches(IInventory inventory, World world) {
+        return this.ingredient.test(inventory.getItem(0));
+    }
+
+    public ItemStack getToastSymbol() {
+        return new ItemStack(Registration.GLASS_PANE_TABLE.get());
+    }
+}
