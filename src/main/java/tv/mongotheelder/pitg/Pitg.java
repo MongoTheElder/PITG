5aab5a6876a38539f20100a67558f466fcfd500e
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:26:34 -0400

    Add GUI texture references

--- src/main/java/tv/mongotheelder/pitg/Pitg.java
+++ src/main/java/tv/mongotheelder/pitg/Pitg.java
@@ -1,5 +1,6 @@
 package tv.mongotheelder.pitg;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -15,12 +16,14 @@ import tv.mongotheelder.pitg.setup.*;
 public class Pitg {
     public static final String GLAZING_TOOL_TAG = "glazing_tool_tag";
     public static final String MODID = "pitg";
+    public static final ResourceLocation GLASS_KILN_GUI_TEXTURE = new ResourceLocation(MODID, "textures/gui/glass_kiln.png");
+    public static final ResourceLocation GLASS_PANE_TABLE_GUI_TEXTURE = new ResourceLocation(MODID, "textures/gui/glass_pane_table.png");
 
-    public static final String[] COLORS = { "", "white", "red", "blue", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "brown", "green", "black" };
+    public static final String[] COLORS = {"", "white", "red", "blue", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "brown", "green", "black"};
 
     public static IProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public Pitg() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
