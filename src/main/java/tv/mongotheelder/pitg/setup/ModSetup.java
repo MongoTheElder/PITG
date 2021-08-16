d7406887f27004ac68ac8aa2c902fb9c31efd118
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:23:32 -0400

    Update to 1.16.5 using Minecraft mappings

--- src/main/java/tv/mongotheelder/pitg/setup/ModSetup.java
+++ src/main/java/tv/mongotheelder/pitg/setup/ModSetup.java
@@ -10,12 +10,12 @@ import tv.mongotheelder.pitg.networking.PacketHandler;
 @Mod.EventBusSubscriber(modid = Pitg.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModSetup {
 
-    public static final ItemGroup ITEM_GROUP = new ItemGroup("pitg") {
+    public static final ItemGroup ITEM_GROUP = (new ItemGroup("pitg") {
         @Override
-        public ItemStack createIcon() {
+        public ItemStack makeIcon() {
             return new ItemStack(Registration.GLASS_PANE.get());
         }
-    };
+    });
 
     public static void init(final FMLCommonSetupEvent event) {
         PacketHandler.registerMessages();
