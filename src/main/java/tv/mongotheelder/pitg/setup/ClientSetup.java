0876c9aa513d9e3568affad81fc9635c4b6f0852
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:22:56 -0400

    Update to 1.16.5 using Minecraft mappings Add support for new pane types

--- src/main/java/tv/mongotheelder/pitg/setup/ClientSetup.java
+++ src/main/java/tv/mongotheelder/pitg/setup/ClientSetup.java
@@ -1,5 +1,6 @@
 package tv.mongotheelder.pitg.setup;
 
+import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.DyeColor;
@@ -11,24 +12,37 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import tv.mongotheelder.pitg.Pitg;
 import tv.mongotheelder.pitg.items.GlazingTool;
 import tv.mongotheelder.pitg.items.GlazingToolMode;
+import tv.mongotheelder.pitg.screens.GlassKilnScreen;
+import tv.mongotheelder.pitg.screens.GlassPaneTableScreen;
 
 @Mod.EventBusSubscriber(modid = Pitg.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientSetup {
 
     public static void init(final FMLClientSetupEvent event) {
-        RenderTypeLookup.setRenderLayer(Registration.GLASS_PANE.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(Registration.DUAL_GLASS_PANE.get(), RenderType.getTranslucent());
+        ScreenManager.register(Registration.GLASS_KILN_CONTAINER.get(), GlassKilnScreen::new);
+        ScreenManager.register(Registration.GLASS_PANE_TABLE_CONTAINER.get(), GlassPaneTableScreen::new);
+
+        RenderTypeLookup.setRenderLayer(Registration.GLASS_PANE.get(), RenderType.cutout());
+        RenderTypeLookup.setRenderLayer(Registration.DUAL_GLASS_PANE.get(), RenderType.cutout());
+        RenderTypeLookup.setRenderLayer(Registration.HORIZONTAL_GLASS_PANE.get(), RenderType.translucent());
 
         for (DyeColor color : DyeColor.values()) {
-            RenderTypeLookup.setRenderLayer(Registration.GLASS_PANES.get(color).get(), RenderType.getTranslucent());
-            RenderTypeLookup.setRenderLayer(Registration.DUAL_GLASS_PANES.get(color).get(), RenderType.getTranslucent());
+            RenderTypeLookup.setRenderLayer(Registration.STAINED_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.TINTED_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.PLAIN_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.STAINED_DUAL_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.TINTED_DUAL_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.PLAIN_DUAL_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.STAINED_HORIZONTAL_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.TINTED_HORIZONTAL_GLASS_PANES.get(color).get(), RenderType.translucent());
+            RenderTypeLookup.setRenderLayer(Registration.PLAIN_HORIZONTAL_GLASS_PANES.get(color).get(), RenderType.translucent());
         }
 
         // Register custom item property for the Glazing Tool to change models for the UNBREAKING state
-        ItemModelsProperties.registerProperty(Registration.GLAZING_TOOL_ITEM.get(), new ResourceLocation("pitg:unbreaking"), (itemStack, clientWorld, livingEntity) -> {
+        ItemModelsProperties.register(Registration.GLAZING_TOOL_ITEM.get(), new ResourceLocation("pitg:unbreaking"), (itemStack, clientWorld, livingEntity) -> {
             if (livingEntity != null) {
-                if (livingEntity.getHeldItemMainhand().getItem() instanceof GlazingTool) {
-                    return livingEntity.getHeldItemMainhand() == itemStack && GlazingTool.getMode(itemStack) == GlazingToolMode.UNBREAKABLE ? 1.0f : 0.0f;
+                if (livingEntity.getMainHandItem().getItem() instanceof GlazingTool) {
+                    return livingEntity.getMainHandItem() == itemStack && GlazingTool.getMode(itemStack) == GlazingToolMode.UNBREAKABLE ? 1.0f : 0.0f;
                 }
             }
             return 0.0F;
