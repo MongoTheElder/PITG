dab10c2052c3c333288f00892f65b3fd718806b6
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:25:50 -0400

    Remove unused 'ENABLE...' settings that were never implemented. Added default config for Glass Kiln burn time

--- src/main/java/tv/mongotheelder/pitg/setup/Config.java
+++ src/main/java/tv/mongotheelder/pitg/setup/Config.java
@@ -8,14 +8,15 @@ import tv.mongotheelder.pitg.items.GlazingToolMode;
 
 @Mod.EventBusSubscriber
 public class Config {
-    public static final String CATEGORY_ITEMS = "glazing_tool";
+    public static final String GLAZING_TOOL_CONFIG = "glazing_tool";
+    public static final String GLASS_KILN_CONFIG = "glass_kiln";
 
     public static ForgeConfigSpec COMMON_CONFIG;
 
-    public static ForgeConfigSpec.BooleanValue ENABLE_GREEN_DYE;
-    public static ForgeConfigSpec.BooleanValue ENABLE_GLAZING_TOOL;
     public static ForgeConfigSpec.BooleanValue ENABLE_PANE_BREAK;
     public static ForgeConfigSpec.BooleanValue ENABLE_UNBREAKABLE;
+
+    public static ForgeConfigSpec.IntValue GLASS_KILN_SMELT_TIME;
 
     static {
         ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
@@ -24,13 +25,16 @@ public class Config {
         COMMON_CONFIG = COMMON_BUILDER.build();
 
     }
-    private static void setupItems(ForgeConfigSpec.Builder COMMON_BUILDER) {
-        COMMON_BUILDER.comment("Glazing Tool settings").push(CATEGORY_ITEMS);
 
-        ENABLE_GLAZING_TOOL = COMMON_BUILDER.comment(" Enable the glazing tool").define("glazing_tool_enabled", true);
+    private static void setupItems(ForgeConfigSpec.Builder COMMON_BUILDER) {
+        COMMON_BUILDER.comment("Glazing Tool settings").push(GLAZING_TOOL_CONFIG);
+
         ENABLE_PANE_BREAK = COMMON_BUILDER.comment(" Let the glazing tool drop panes (like silk touch) by sneaking while in BREAK mode").define("glazing_tool_break_pane", false);
-        ENABLE_UNBREAKABLE= COMMON_BUILDER.comment(" Allow glazing tool to mark glass panes as unbreakable while in UNBREAKING mode").define("unbreakable_pane", false);
-        ENABLE_GREEN_DYE = COMMON_BUILDER.comment(" Enable alternate green dye").define("green_dye_enabled", true);
+        ENABLE_UNBREAKABLE = COMMON_BUILDER.comment(" Allow glazing tool to mark glass panes as unbreakable while in UNBREAKING mode").define("unbreakable_pane", false);
+
+        COMMON_BUILDER.pop().comment("Glass Kiln settings").push(GLASS_KILN_CONFIG);
+
+        GLASS_KILN_SMELT_TIME = COMMON_BUILDER.comment(" Item smelt time (ticks)").defineInRange("glass_furnace_smelt_time", 200, 20, 5000);
 
         COMMON_BUILDER.pop();
     }
