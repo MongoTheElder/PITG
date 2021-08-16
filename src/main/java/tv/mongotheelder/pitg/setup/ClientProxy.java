f4114f795ada7e48c03bcda57fccb6964d610609
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:20:57 -0400

    Update to 1.16.5 using Minecraft mappings

--- src/main/java/tv/mongotheelder/pitg/setup/ClientProxy.java
+++ src/main/java/tv/mongotheelder/pitg/setup/ClientProxy.java
@@ -7,6 +7,6 @@ public class ClientProxy implements IProxy {
 
     @Override
     public World getClientWorld() {
-        return Minecraft.getInstance().world;
+        return Minecraft.getInstance().level;
     }
 }
