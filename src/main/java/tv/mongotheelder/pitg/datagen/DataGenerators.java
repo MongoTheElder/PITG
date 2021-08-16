87ecb935f730a7120d1bae25550a301300b3d4cb
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 12:04:06 -0400

    Add Tags and Item providers Refactor provider names to de-conflict collisions

--- src/main/java/tv/mongotheelder/pitg/datagen/DataGenerators.java
+++ src/main/java/tv/mongotheelder/pitg/datagen/DataGenerators.java
@@ -1,9 +1,11 @@
 package tv.mongotheelder.pitg.datagen;
 
+import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import tv.mongotheelder.pitg.Pitg;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
@@ -12,11 +14,13 @@ public class DataGenerators {
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         if (event.includeServer()) {
-            generator.addProvider(new Recipes(generator));
-            generator.addProvider(new LootTables(generator));
+            generator.addProvider(new PitgRecipeProvider(generator));
+            generator.addProvider(new PitgLootTableProvider(generator));
+            generator.addProvider(new PitgItemsTagsProvider(generator, new BlockTagsProvider(generator, Pitg.MODID, event.getExistingFileHelper()), event.getExistingFileHelper()));
         }
         if (event.includeClient()) {
-            generator.addProvider(new BlockStates(generator, event.getExistingFileHelper()));
+            generator.addProvider(new PitgBlockStateProvider(generator, event.getExistingFileHelper()));
+            generator.addProvider(new PitgItemProvider(generator, event.getExistingFileHelper()));
         }
     }
 }
