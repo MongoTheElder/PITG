955e129768bf5310f3f7f8224603bfde9295c944
MongoTheElder <mongotheelder@gmail.com>
2021-08-16 11:59:53 -0400

    Update to 1.16.5 using Minecraft mappings

--- src/main/java/tv/mongotheelder/pitg/blocks/StainedGlassPane.java
+++ src/main/java/tv/mongotheelder/pitg/blocks/StainedGlassPane.java
@@ -1,19 +1,19 @@
 package tv.mongotheelder.pitg.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.item.DyeColor;
 
 public class StainedGlassPane extends GlassPane {
     private final DyeColor color;
 
-    public StainedGlassPane(DyeColor colorIn, Block.Properties properties) {
+    public StainedGlassPane(DyeColor colorIn, AbstractBlock.Properties properties) {
         super(properties);
         this.color = colorIn;
-        this.setDefaultState(this.stateContainer.getBaseState()
-                .with(NORTH, Boolean.FALSE)
-                .with(EAST, Boolean.FALSE)
-                .with(SOUTH, Boolean.FALSE)
-                .with(WEST, Boolean.FALSE)
-                .with(WATERLOGGED, Boolean.FALSE));
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(NORTH, Boolean.FALSE)
+                .setValue(EAST, Boolean.FALSE)
+                .setValue(SOUTH, Boolean.FALSE)
+                .setValue(WEST, Boolean.FALSE)
+                .setValue(WATERLOGGED, Boolean.FALSE));
     }
 }
